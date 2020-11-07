ed8ebb0eb6c713cc499c5e3892c698c9317db5e8
Camila Yánez <66316981+CamiYanez@users.noreply.github.com>
2020-11-06 21:38:41 -0500

    cambio

--- src/responsive.js
+++ src/responsive.js
@@ -3,6 +3,7 @@ let mediaqueryList = window.matchMedia("(min-width: 992px)");
 function manejador() {
     if (mediaqueryList.matches) {
         alert('La media query ahora se cumple');
+        filter(50);
     }
 }
 
